3fbf38e60e0802a08353e6f06f7ef9d7e04bf673
Pietro Griffa <pietro.griffa@mail.polimi.it>
2019-11-11 22:07:28 +0100

    comemment migration

--- code/migration.m
+++ code/migration.m
@@ -1,22 +1,29 @@
 function [] = migration(world,migration)
+% Implementation of the migration game.
+%
+% Inputs:
+%   - world: structure with all the info about the evolution of teh system
+%   - migration: structure with all the options of interest for the
+%           specific type of game
+%
+% Outputs:
+%
 
-populatedslots = find(world.composition);
-focal_idx = randsample(populatedslots, world.N);
+populatedslots = find(world.composition);   % identify populated slots
+focal_idx = randsample(populatedslots, world.N); % sample players from population
 
-[row,col] = ind2sub(size(world.composition),focal_idx);
+[row,col] = ind2sub(size(world.composition),focal_idx); % find coordinates from index
 mat_players = [row col];
 
+% Convert the matrix of the coordinates of the players for the game in a
+% cell array (num_players x 1).
+% (This way we can call the function to compute the new payoff easily for
+% all players through cellfun).
 cell_players = mat2cell(mat_players,[ones(1,size(mat_players,1))],[size(mat_players,2)]);
 
-
-v = [-migration.neighb:1:migration.neighb];
-combs = unique(sort(nchoosek(repmat(v,1,k),k),2),'rows');
-for i=1:size(combs,1)
-    
+% Compute new payoffs for players (and update payoffs map)
+game_fun =@(p_idx) neighborhood_watch(world, migration,p_idx);
+playoffs = cellfun(game_fun,cell_players);
     
     
 end
-
-[row,col] = ind2sub(size(world.composition),idx);
-
-C = combnk(v,k)
--- code/neighborhood_watch.m
+++ code/neighborhood_watch.m
@@ -1,5 +1,15 @@
-function [payoff] = neighborhood_watch(world, migration,player_idx)
+function [payoff] = neighborhood_watch(world, migration,player_cord)
 % Looks around: gets the sum of neighboring payoffs
+%
+% Inputs:
+%   - world: structure with all the info about the evolution of teh system
+%   - migration: structure with all the options of interest for the
+%           specific type of game
+%   - player_cord: coordinates of the focal player
+%
+% Outputs:
+%   - payoff: new payoff of the focal player
+%
 
 % We want to observe the neighborhood of size world.m: to identify the
 % points in this area we sum to the coordinates of the focal point the
@@ -9,11 +19,11 @@ v = [-migration.m:1:migration.m];
 combs = unique(sort(nchoosek(repmat(v,1,k),k),2),'rows');
 combs = combs(sum(combs,2)~=0,:);   % remove central point
 
-to_check = player_idx+combs;    % indeces of points around focal we want to check
+to_check = player_cord+combs;    % indeces of points around focal we want to check
 rows = to_check(:,1);       columns = to_check(:,2);
 idx2check = sub2ind(size(world.composition),rows,columns);  % switch form coordinates to indeces
 
-player_strategy = world.composition(player_idx);    % strategy of focal player
+player_strategy = world.composition(player_cord);    % strategy of focal player
 neighbors_strategy = world.composition(idx2check);  % strategy of neighboring player
 neighbors_strategy = neighbors_strategy(neighbors_strategy~=0); % remove free slots
 
