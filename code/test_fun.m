cdb1c98b3a9fc8f6bd4199fd7f6ad6812fabbc84
Pietro Griffa <pietro.griffa@mail.polimi.it>
2019-12-02 23:44:01 +0100

    Cleaned

--- code/test_fun.m
+++
@@ -1,4 +0,0 @@
-function [b] = test_fun(a)  % [inputs], (outputs)
-   % if I want to change the struct parameters globally, I have to also
-   % passe them as outputs in addition to the struct itself
-b = a +2;
